9a93a83606b9f5c98411be36bd722ccc70e4b652
Glenn <glenn.fiedler@gmail.com>
2016-12-29 17:29:27 -0800

    sketched out server api

--- speednet.c
+++ speednet.c
@@ -80,6 +80,15 @@ void speednet_client_update( struct speednet_client_t * client, double time )
 	// ...
 }
 
+int speednet_client_get_client_index( struct speednet_client_t * client )
+{
+	(void) client;
+
+	// ...
+
+	return -1;
+}
+
 void speednet_client_connect( struct speednet_client_t * client, const char * token )
 {
 	(void) client;
@@ -119,5 +128,96 @@ void speednet_client_destroy( struct speednet_client_t * client )
 {
 	(void) client;
 
+	// todo: destroy it
+}
+
+struct speednet_server_t
+{
+	int dummy;
+};
+
+struct speednet_server_t * speednet_server_create( uint16_t port )
+{
+	(void) port;
+
+	// todo
+
+	return NULL;
+}
+
+void speednet_server_start( struct speednet_server_t * server, int max_clients )
+{
+	(void) server;
+	(void) max_clients;
+
 	// ...
+}
+
+void speednet_server_update( struct speednet_server_t * server, double time )
+{
+	(void) server;
+	(void) time;
+
+	// ...
+}
+
+int speednet_server_is_client_connected( struct speednet_server_t * server, int client_index )
+{
+	(void) server;
+	(void) client_index;
+
+	// ...
+
+	return 0;
+}
+
+int speednet_server_receive_packet_from_client( struct speednet_server_t * server, int client_index, uint8_t * buffer, int buffer_length )
+{
+	(void) server;
+	(void) client_index;
+	(void) buffer;
+	(void) buffer_length;
+
+	// ...
+
+	return 0;
+}
+
+void speednet_server_send_packet_to_client( struct speednet_server_t * server, int client_index, const uint8_t * packet_data, int packet_size )
+{
+	(void) server;
+	(void) client_index;
+	(void) packet_data;
+	(void) packet_size;
+
+	// ...
+}
+
+void speednet_server_disconnect_client( struct speednet_server_t * server, int client_index )
+{
+	(void) server;
+	(void) client_index;
+
+	// ...
+}
+
+void speednet_server_disconnect_all_clients( struct speednet_server_t * server )
+{
+	(void) server;
+
+	// ...
+}
+
+void speednet_server_stop( struct speednet_server_t * server )
+{
+	(void) server;
+
+	// ...
+}
+
+void speednet_server_destroy( struct speednet_server_t * server )
+{
+	(void) server;
+
+	// todo: destroy
 }
