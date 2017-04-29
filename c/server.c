d2ce6e4b8707e07902d61d828767f6d895ccb375
Glenn Fiedler <glenn.fiedler@gmail.com>
2017-04-28 19:22:31 -0700

    fix server port

--- c/server.c
+++ c/server.c
@@ -66,6 +66,9 @@ int main( int argc, char ** argv )
     if ( argc == 2 )
         server_address = argv[1];
 
+    if ( server_address.port == 0 )
+        server_address.port = 40000;
+
     struct netcode_server_t * server = netcode_server_create( server_address, TEST_PROTOCOL_ID, private_key, time );
 
     if ( !server )
