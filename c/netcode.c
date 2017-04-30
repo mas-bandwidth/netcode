1c16eab83022ab965e3df3b23267f959ebf9b438
Glenn Fiedler <glenn.fiedler@gmail.com>
2017-04-30 10:03:01 -0700

    remove some unintentional duplication

--- c/netcode.c
+++ c/netcode.c
@@ -39,32 +39,6 @@
 
 #include <sodium.h>
 
-#if    defined(__386__) || defined(i386)    || defined(__i386__)  \
-    || defined(__X86)   || defined(_M_IX86)                       \
-    || defined(_M_X64)  || defined(__x86_64__)                    \
-    || defined(alpha)   || defined(__alpha) || defined(__alpha__) \
-    || defined(_M_ALPHA)                                          \
-    || defined(ARM)     || defined(_ARM)    || defined(__arm__)   \
-    || defined(WIN32)   || defined(_WIN32)  || defined(__WIN32__) \
-    || defined(_WIN32_WCE) || defined(__NT__)                     \
-    || defined(__MIPSEL__)
-  #define NETCODE_LITTLE_ENDIAN 1
-#else
-  #define NETCODE_BIG_ENDIAN 1
-#endif
-
-#define NETCODE_PLATFORM_WINDOWS    1
-#define NETCODE_PLATFORM_MAC        2
-#define NETCODE_PLATFORM_UNIX       3
-
-#if defined(_WIN32)
-#define NETCODE_PLATFORM NETCODE_PLATFORM_WINDOWS
-#elif defined(__APPLE__)
-#define NETCODE_PLATFORM NETCODE_PLATFORM_MAC
-#else
-#define NETCODE_PLATFORM NETCODE_PLATFORM_UNIX
-#endif
-
 #define NETCODE_SOCKET_IPV6         1
 #define NETCODE_SOCKET_IPV4         2
 
